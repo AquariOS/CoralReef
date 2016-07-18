DirtyUnicorns/android_packages_apps_DU-Tweaks

Navigation: All the bars have a Pulse! [3/4]
Move Pulse settings out of Fling and into a new fragment
under global navigation bar settings.

Change-Id: I2cc5b1e44a57395e799214fb81b9ba2ad2e48a97

src/com/dirtyunicorns/dutweaks/fragments/PulseSettings.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.dutweaks.fragments;
+
+import java.util.ArrayList;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = PulseSettings.class.getSimpleName();
+
+    SwitchPreference mShowPulse;
+    SwitchPreference mLavaLampEnabled;
+    ColorPickerPreference mPulseColor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pulse_settings);
+
+        ActionBar bar = getActivity().getActionBar();
+        if (bar != null) {
+            bar.setTitle(R.string.pulse_settings);
+        }
+
+        mShowPulse = (SwitchPreference) findPreference("eos_fling_show_pulse");
+        mShowPulse.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.FLING_PULSE_ENABLED, 1) == 1);
+        mShowPulse.setOnPreferenceChangeListener(this);
+
+        int pulseColor = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.FLING_PULSE_COLOR, Color.WHITE, UserHandle.USER_CURRENT);
+        mPulseColor = (ColorPickerPreference) findPreference("eos_fling_pulse_color");
+        mPulseColor.setNewPreviewColor(pulseColor);
+        mPulseColor.setOnPreferenceChangeListener(this);
+
+        mLavaLampEnabled = (SwitchPreference) findPreference("eos_fling_lavalamp");
+        mLavaLampEnabled.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, 1) == 1);
+        mLavaLampEnabled.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mShowPulse)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_ENABLED, enabled ? 1 : 0);
+            return true;
+        } else if (preference.equals(mPulseColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_COLOR, color);
+            return true;
+        } else if (preference.equals(mLavaLampEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, enabled ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DIRTYTWEAKS;
+    }
+}

AquariOS/CoralReef

Conflicts:
	res/values/strings.xml
	res/xml/navbar_settings.xml
	src/com/android/settings/custom/nav/NavbarSettings.java

src/com/citrus/settings/fragments/PulseSettings.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.citrus.settings.fragments;
+
+import java.util.ArrayList;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = PulseSettings.class.getSimpleName();
+
+    SwitchPreference mShowPulse;
+    SwitchPreference mLavaLampEnabled;
+    ColorPickerPreference mPulseColor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pulse_settings);
+
+        ActionBar bar = getActivity().getActionBar();
+        if (bar != null) {
+            bar.setTitle(R.string.pulse_settings);
+        }
+
+        mShowPulse = (SwitchPreference) findPreference("eos_fling_show_pulse");
+        mShowPulse.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.FLING_PULSE_ENABLED, 1) == 1);
+        mShowPulse.setOnPreferenceChangeListener(this);
+
+        int pulseColor = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.FLING_PULSE_COLOR, Color.WHITE, UserHandle.USER_CURRENT);
+        mPulseColor = (ColorPickerPreference) findPreference("eos_fling_pulse_color");
+        mPulseColor.setNewPreviewColor(pulseColor);
+        mPulseColor.setOnPreferenceChangeListener(this);
+
+        mLavaLampEnabled = (SwitchPreference) findPreference("eos_fling_lavalamp");
+        mLavaLampEnabled.setChecked(Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, 1) == 1);
+        mLavaLampEnabled.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mShowPulse)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_ENABLED, enabled ? 1 : 0);
+            return true;
+        } else if (preference.equals(mPulseColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_COLOR, color);
+            return true;
+        } else if (preference.equals(mLavaLampEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, enabled ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.CITRUS_SETTINGS;
+    }
+}