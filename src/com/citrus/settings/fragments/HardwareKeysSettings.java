Citrus-CAF/packages_apps_CitrusSettings

Buttons: Hardware key disable support with M adaptations [2/2]
Change-Id: If730a4720d0c2b0906afcf3b14520e6f7d777348

src/com/citrus/settings/fragments/HardwareKeysSettings.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.UserHandle; 
 import android.preference.SwitchPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -43,11 +44,11 @@ import com.android.internal.util.slim.ActionConstants;
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
 import com.android.internal.util.slim.HwKeyHelper;
+import com.android.internal.utils.du.DUActionUtils; 
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import com.citrus.settings.fragments.ButtonBacklightBrightness; 
-import com.citrus.settings.util.ShortcutPickerHelper;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -59,7 +60,10 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "HardwareKeys";
 
+    private static final String HWKEY_DISABLE = "hardware_keys_disable"; 
+
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight"; 
+    private static final String CATEGORY_HWKEY = "hardware_keys"; 
     private static final String CATEGORY_KEYS = "button_keys";
     private static final String CATEGORY_BACK = "button_keys_back";
     private static final String CATEGORY_CAMERA = "button_keys_camera";
@@ -105,6 +109,7 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
     private static final int KEY_MASK_CAMERA     = 0x20;
 
     private SwitchPreference mEnableCustomBindings;
+    private SwitchPreference mHwKeyDisable; 
     private Preference mBackPressAction;
     private Preference mBackLongPressAction;
     private Preference mBackDoubleTapAction;
@@ -227,6 +232,21 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
                 KEYS_APP_SWITCH_LONG_PRESS);
         mAppSwitchDoubleTapAction = (Preference) prefs.findPreference(
                 KEYS_APP_SWITCH_DOUBLE_TAP);
+
+      final boolean needsNavbar = DUActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+        } else {
+            prefs.removePreference(hwkeyCat);
+        }
 
         if (hasBackKey) {
             // Back key
@@ -369,7 +389,11 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
         mCheckPreferences = true;
         return prefs;
     }
-    
+
+       // load preferences first
+       setActionPreferencesEnabled(keysDisabled == 0);
+    }
+
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.APPLICATION;
@@ -482,6 +506,12 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), Settings.System.HARDWARE_KEY_REBINDING,
                     value ? 1 : 0);
+            return true;
+        } else if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
             return true;
         }
         return false;

AquariOS/CoralReef

src/com/citrus/settings/fragments/HardwareKeysSettings.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.UserHandle; 
 import android.preference.SwitchPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -43,23 +44,27 @@ import com.android.internal.util.slim.ActionConstants;
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
 import com.android.internal.util.slim.HwKeyHelper;
+import com.android.internal.utils.du.DUActionUtils; 
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import com.citrus.settings.fragments.ButtonBacklightBrightness; 
-import com.citrus.settings.util.ShortcutPickerHelper;
+import com.citrus.settings.util.ShortcutPickerHelper; 
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class HardwareKeysSettings extends SettingsPreferenceFragment implements
+public class HardwareKeysSettings extends ActionFragment implements
         OnPreferenceChangeListener, OnPreferenceClickListener,
         ShortcutPickerHelper.OnPickListener {
 
     private static final String TAG = "HardwareKeys";
 
+    private static final String HWKEY_DISABLE = "hardware_keys_disable"; 
+
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight"; 
+    private static final String CATEGORY_HWKEY = "hardware_keys"; 
     private static final String CATEGORY_KEYS = "button_keys";
     private static final String CATEGORY_BACK = "button_keys_back";
     private static final String CATEGORY_CAMERA = "button_keys_camera";
@@ -105,6 +110,7 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
     private static final int KEY_MASK_CAMERA     = 0x20;
 
     private SwitchPreference mEnableCustomBindings;
+    private SwitchPreference mHwKeyDisable; 
     private Preference mBackPressAction;
     private Preference mBackLongPressAction;
     private Preference mBackDoubleTapAction;
@@ -227,6 +233,24 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
                 KEYS_APP_SWITCH_LONG_PRESS);
         mAppSwitchDoubleTapAction = (Preference) prefs.findPreference(
                 KEYS_APP_SWITCH_DOUBLE_TAP);
+
+      final boolean needsNavbar = DUActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefs
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+        } else {
+            prefs.removePreference(hwkeyCat);
+
+           // load preferences first
+           setActionPreferencesEnabled(keysDisabled == 0);
+        }
 
         if (hasBackKey) {
             // Back key
@@ -369,7 +393,7 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
         mCheckPreferences = true;
         return prefs;
     }
-    
+
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.APPLICATION;
@@ -482,6 +506,12 @@ public class HardwareKeysSettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), Settings.System.HARDWARE_KEY_REBINDING,
                     value ? 1 : 0);
+            return true;
+        } else if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
             return true;
         }
         return false;