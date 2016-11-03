5c252d1e8a116a217f1b0182f5bcecad0bf35b64
Lars Greiss <kufikugel@googlemail.com>
2016-11-03 15:02:19 +0800

    HeadsUp: add timeout option (2/2)
    Forward ported to marshmallow/nougat By: BeansTown106
    removed the none option as we now have a headsup minimum time of 2seconds
    added 5sec option as that is the new default in marshmallow
    
    Change-Id: Ieed3d0110110f770b299d6733e588249489adda0

--- src/com/aquarios/coralreef/fragments/HeadsUp.java
+++ src/com/aquarios/coralreef/fragments/HeadsUp.java
@@ -24,9 +24,11 @@ import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
@@ -57,10 +59,11 @@ import java.util.List;
 import java.util.Map;
 
 public class HeadsUp extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
+        Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener, Indexable {
 
     private static final int DIALOG_STOPLIST_APPS = 0;
     private static final int DIALOG_BLACKLIST_APPS = 1;
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
     private PackageListAdapter mPackageAdapter;
     private PackageManager mPackageManager;
@@ -68,6 +71,7 @@ public class HeadsUp extends SettingsPreferenceFragment implements
     private PreferenceGroup mBlacklistPrefList;
     private Preference mAddStoplistPref;
     private Preference mAddBlacklistPref;
+    private ListPreference mHeadsUpTimeOut;
 
     private String mStoplistPackageList;
     private String mBlacklistPackageList;
@@ -96,6 +100,28 @@ public class HeadsUp extends SettingsPreferenceFragment implements
 
         mAddStoplistPref.setOnPreferenceClickListener(this);
         mAddBlacklistPref.setOnPreferenceClickListener(this);
+
+        Resources systemUiResources;
+        try {
+            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            return;
+        }
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
     }
 
     @Override
@@ -109,7 +135,16 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.AQUA;
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT,
+                    headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
+        }
         return false;
     }
 
