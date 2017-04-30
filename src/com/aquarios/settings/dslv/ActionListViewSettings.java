calebcabob/TheAquarium

refractor to aquarios

src/com/aquarios/settings/dslv/ActionListViewSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.dslv;
+package com.aquarios.settings.dslv;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -53,18 +53,18 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.internal.util.aosip.ActionConfig;
-import com.android.internal.util.aosip.ActionConstants;
-import com.android.internal.util.aosip.ActionHelper;
-import com.android.internal.util.aosip.ImageHelper;
-import com.android.internal.util.aosip.DeviceUtils;
-import com.android.internal.util.aosip.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.aquarios.ActionConfig;
+import com.android.internal.util.aquarios.ActionConstants;
+import com.android.internal.util.aquarios.ActionHelper;
+import com.android.internal.util.aquarios.ImageHelper;
+import com.android.internal.util.aquarios.DeviceUtils;
+import com.android.internal.util.aquarios.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import com.aosip.owlsnest.dslv.DragSortListView;
-import com.aosip.owlsnest.dslv.DragSortController;
-import com.aosip.owlsnest.utils.ShortcutPickerHelper;
+import com.aquarios.settings.dslv.DragSortListView;
+import com.aquarios.settings.dslv.DragSortController;
+import com.aquarios.settings.utils.ShortcutPickerHelper;
 import com.android.settings.Utils;
 
 import java.io.File;

src/com/aquarios/settings/utils/ShortcutPickerHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.utils;
+package com.aquarios.settings.utils;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -31,7 +31,7 @@ import android.util.Log;
 import com.android.settings.Utils;
 import com.android.settings.R;
 
-import com.android.internal.util.aosip.AppHelper;
+import com.android.internal.util.aquarios.AppHelper;
 import java.util.ArrayList;
 
 public class ShortcutPickerHelper {

AquariOS/CoralReef

src/com/aquarios/settings/dslv/ActionListViewSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.dslv;
+package com.aquarios.settings.dslv;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -53,18 +53,18 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.internal.util.aosip.ActionConfig;
-import com.android.internal.util.aosip.ActionConstants;
-import com.android.internal.util.aosip.ActionHelper;
-import com.android.internal.util.aosip.ImageHelper;
-import com.android.internal.util.aosip.DeviceUtils;
-import com.android.internal.util.aosip.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.slim.ActionConfig;
+import com.android.internal.util.slim.ActionConstants;
+import com.android.internal.util.slim.ActionHelper;
+import com.android.internal.util.slim.ImageHelper;
+import com.android.internal.util.slim.DeviceUtils;
+import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import com.aosip.owlsnest.dslv.DragSortListView;
-import com.aosip.owlsnest.dslv.DragSortController;
-import com.aosip.owlsnest.utils.ShortcutPickerHelper;
+import com.aquarios.settings.dslv.DragSortListView;
+import com.aquarios.settings.dslv.DragSortController;
+import com.aquarios.settings.utils.ShortcutPickerHelper;
 import com.android.settings.Utils;
 
 import java.io.File;

src/com/aquarios/settings/utils/ShortcutPickerHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aosip.owlsnest.utils;
+package com.aquarios.settings.utils;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -31,7 +31,7 @@ import android.util.Log;
 import com.android.settings.Utils;
 import com.android.settings.R;
 
-import com.android.internal.util.aosip.AppHelper;
+import com.android.internal.util.slim.AppHelper;
 import java.util.ArrayList;
 
 public class ShortcutPickerHelper {