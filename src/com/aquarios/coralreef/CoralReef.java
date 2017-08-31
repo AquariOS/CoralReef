2d313daf91ced982400f81288b9be76e4b1ce3a2
calebcabob <calphonic@gmail.com>
2017-08-31 17:22:30 -0400

    Coral Reef: Dive Deep Into AquariOS
    Clean base with the following tabs:
    * Buttons
    * Lock Screen
    * Status Bar
    * Quick Settings
    * Notifications
    * System/Misc
    
    All based on...
    https://github.com/Citrus-CAF/packages_apps_CustomSquash/commit/b0710a538d44125a565898899713e60852dcbd40

---
+++ src/com/aquarios/coralreef/CoralReef.java
@@ -0,0 +1,124 @@
+package com.aquarios.coralreef;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.aquarios.coralreef.PagerSlidingTabStrip;
+
+import com.aquarios.coralreef.tabs.ButtonsTab;
+import com.aquarios.coralreef.tabs.LockScreenTab;
+import com.aquarios.coralreef.tabs.QuickSettingsTab;
+import com.aquarios.coralreef.tabs.StatusBarTab;
+import com.aquarios.coralreef.tabs.NotificationsTab;
+import com.aquarios.coralreef.tabs.SystemMiscTab;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoralReef extends SettingsPreferenceFragment {
+
+    ViewPager mViewPager;
+    String titleString[];
+    ViewGroup mContainer;
+    PagerSlidingTabStrip mTabs;
+
+    static Bundle mSavedState;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContainer = container;
+        View view = inflater.inflate(R.layout.coral_reef, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.pager);
+	mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+
+        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
+        mViewPager.setAdapter(StatusBarAdapter);
+
+	mTabs.setViewPager(mViewPager);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+   @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+         mContainer.setPadding(30, 30, 30, 30);
+    }
+
+    class StatusBarAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public StatusBarAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new ButtonsTab();
+            frags[1] = new LockScreenTab();
+            frags[2] = new QuickSettingsTab();
+            frags[3] = new StatusBarTab();
+            frags[4] = new NotificationsTab();
+            frags[5] = new SystemMiscTab();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+            getString(R.string.buttons_tab_title),
+            getString(R.string.lockscreen_tab_title),
+            getString(R.string.quick_settings_tab_title),
+            getString(R.string.status_bar_tab_title),
+            getString(R.string.notifications_tab_title),
+            getString(R.string.system_misc_tab_title)};
+        return titleString;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AQUA;
+    }
+}
