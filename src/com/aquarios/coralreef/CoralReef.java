4a4552480817f4e8bd984bcb05369eb85ba8d559
calebcabob <calphonic@gmail.com>
2017-12-14 00:01:16 -0500

    Refractor this gem for AquariOS
    First off, a HUGE thanks to @madza (Alex Cruz) and the Dirty Unicorns team for the countless hours that went into the development of this new custom rom settings interface/system, and continues to go into their amazing project! Also, I want to express my gratitude for their willingness to continue to openly share their work for other devs to use, such as "DU-Tweaks".
    
        -I take absolutely no credit for this, because I've just simply renamed what they created in order for it to work with AquariOS 8.1.
    
        -Used the preferences from Omni found here: https://github.com/omnirom/android_packages_apps_OmniGears/commit/f9e3b2176f6e2a14810d82f85c34eec85924cbae
    
        -Created an additional tab in CoralReef in order to maintain a similar structure to what we've been using.
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

---
+++ src/com/aquarios/coralreef/CoralReef.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2017 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aquarios.coralreef;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.aquarios.coralreef.navigation.BottomNavigationViewCustom;
+import com.aquarios.coralreef.tabs.ButtonsTab;
+import com.aquarios.coralreef.tabs.LockScreenTab;
+import com.aquarios.coralreef.tabs.QuickSettingsTab;
+import com.aquarios.coralreef.tabs.StatusBarTab;
+import com.aquarios.coralreef.tabs.NotificationsTab;
+import com.aquarios.coralreef.tabs.SystemMiscTab;
+
+public class CoralReef extends SettingsPreferenceFragment {
+
+    public CoralReef() {
+    }
+
+    MenuItem menuitem;
+
+    PagerAdapter mPagerAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.coralreef, container, false);
+
+        final BottomNavigationViewCustom navigation = view.findViewById(R.id.navigation);
+
+        final ViewPager viewPager = view.findViewById(R.id.viewpager);
+
+        navigation.setBackground(new ColorDrawable(getResources().getColor(R.color.BottomBarBackgroundColor)));
+
+        mPagerAdapter = new PagerAdapter(getFragmentManager());
+        viewPager.setAdapter(mPagerAdapter);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationViewCustom.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.buttons_tab:
+                        viewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.lock_screen_tab:
+                        viewPager.setCurrentItem(1);
+                        return true;
+                    case R.id.quick_settings_tab:
+                        viewPager.setCurrentItem(2);
+                        return true;
+                    case R.id.status_bar_tab:
+                        viewPager.setCurrentItem(3);
+                        return true;
+                    case R.id.notifications_tab:
+                        viewPager.setCurrentItem(4);
+                        return true;
+                    case R.id.system_misc_tab:
+                        viewPager.setCurrentItem(4);
+                        return true;
+                }
+                return false;
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(menuitem != null) {
+                    menuitem.setChecked(false);
+                } else {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                navigation.getMenu().getItem(position).setChecked(true);
+                menuitem = navigation.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        return view;
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new ButtonsTab();
+            frags[1] = new LockScreenTab();
+            frags[2] = new QuickSettingsTab();
+            frags[3] = new StatusBarTab();
+            frags[4] = new NotificationsTab();
+            frags[4] = new SystemMiscTab();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.bottom_nav_buttons_title),
+                getString(R.string.bottom_nav_lock_screen_title),
+                getString(R.string.bottom_nav_quick_settings_title),
+                getString(R.string.bottom_nav_status_bar_title),
+                getString(R.string.bottom_nav_notifications_title),
+                getString(R.string.bottom_nav_system_misc_title)};
+
+        return titleString;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AQUA;
+    }
+}
