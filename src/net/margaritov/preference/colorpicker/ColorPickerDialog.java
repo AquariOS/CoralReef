9d58bbbf03748edd1f9d67de3a6ad5e034e00441
rc420head <dennislebhardt79@gmail.com>
2018-02-08 08:12:33 -0600

    add missing import

--- src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
