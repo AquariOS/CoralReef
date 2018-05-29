ff0fdf23bba0fe7749aff036071bd516d038bf8d
calebcabob <calphonic@gmail.com>
2018-05-29 11:39:10 -0400

    Add Black theme stuff to AccentPicker (2/3)
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- src/com/aquarios/coralreef/helpers/AccentPicker.java
+++ src/com/aquarios/coralreef/helpers/AccentPicker.java
@@ -369,9 +369,9 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
             blackAccent = mView.findViewById(R.id.blackAccent);
             // Change the accent picker button depending on whether or not the dark theme is applied
             blackAccent.setBackgroundColor(getResources().getColor(
-                    isUsingDarkTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
+                    isUsingDarkTheme() || isUsingBlackAFTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
             blackAccent.setBackgroundTintList(getResources().getColorStateList(
-                    isUsingDarkTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
+                    isUsingDarkTheme() || isUsingBlackAFTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
         }
         if (blackAccent != null) {
             blackAccent.setOnClickListener(new View.OnClickListener() {
@@ -404,6 +404,18 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
         OverlayInfo themeInfo = null;
         try {
             themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
+                    UserHandle.USER_CURRENT);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
+
+    // Check for the blackaf theme overlay
+    private boolean isUsingBlackAFTheme() {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.blackaf",
                     UserHandle.USER_CURRENT);
         } catch (RemoteException e) {
             e.printStackTrace();
