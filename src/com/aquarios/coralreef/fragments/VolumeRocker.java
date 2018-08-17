e52415bfd5254c8e85cb70beab9cfb39821ec41c
ezio84 <brabus84@gmail.com>
2018-08-17 02:31:10 -0700

    CoralReef: Double tap/hold power to toggle flashlight [2/3]
    Adin's edit: added toast notifying jump to camera disable
    
    Change-Id: I9282e2d2426af5901b5e31895b5e4ce3479b7b20

--- src/com/aquarios/coralreef/fragments/VolumeRocker.java
+++ src/com/aquarios/coralreef/fragments/VolumeRocker.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.widget.Toast;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -27,6 +28,7 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 
+import com.android.internal.util.aquarios.AquaUtils;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
@@ -41,14 +43,40 @@ import java.util.List;
 public class VolumeRocker extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.volume_rocker);
+
+         if (!AquaUtils.deviceSupportsFlashLight(getContext())) {
+            Preference toRemove = (Preference) prefSet.findPreference(TORCH_POWER_BUTTON_GESTURE);
+            if (toRemove != null) {
+                keysCategory.removePreference(toRemove);
+            }
+        } else {
+            mTorchPowerButton = (ListPreference) findPreference(TORCH_POWER_BUTTON_GESTURE);
+            mTorchPowerButton.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mTorchPowerButton) {
+            int torchPowerButtonValue = Integer.valueOf((String) objValue);
+            boolean doubleTapCameraGesture = Settings.Secure.getInt(mResolver,
+                    Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 0) == 0;
+            if (torchPowerButtonValue == 1 && doubleTapCameraGesture) {
+                // if double-tap for torch is enabled, switch off double-tap for camera
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1);
+                Toast.makeText(getActivity(),
+                        (R.string.torch_power_button_gesture_dt_toast),
+                        Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
         return false;
     }
 
