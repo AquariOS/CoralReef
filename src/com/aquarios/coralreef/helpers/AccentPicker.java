ff53a36a51f2f2946b6e56458f64708e58d70fc9
Shubham Singh <coolsks94@gmail.com>
2018-10-11 04:28:08 -0400

    Themes: Add a Black UI option to built-in themer [3/4]
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- src/com/aquarios/coralreef/helpers/AccentPicker.java
+++ src/com/aquarios/coralreef/helpers/AccentPicker.java
@@ -367,11 +367,11 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
         Button blackAccent = null;
         if (mView != null) {
             blackAccent = mView.findViewById(R.id.blackAccent);
-            // Change the accent picker button depending on whether or not the dark theme is applied
+            // Change the accent picker button depending on whether or not the dark or black theme is applied
             blackAccent.setBackgroundColor(getResources().getColor(
-                    isUsingDarkTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
+                    (isUsingDarkTheme() || isUsingBlackTheme()) ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
             blackAccent.setBackgroundTintList(getResources().getColorStateList(
-                    isUsingDarkTheme() ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
+                    (isUsingDarkTheme() || isUsingBlackTheme()) ? R.color.accent_picker_white_accent : R.color.accent_picker_dark_accent));
         }
         if (blackAccent != null) {
             blackAccent.setOnClickListener(new View.OnClickListener() {
@@ -410,6 +410,18 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
         }
         return themeInfo != null && themeInfo.isEnabled();
     }
+
+     // Check for the black theme overlay
+     private boolean isUsingBlackTheme() {
+         OverlayInfo themeInfo = null;
+         try {
+             themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black",
+                     UserHandle.USER_CURRENT);
+         } catch (RemoteException e) {
+             e.printStackTrace();
+         }
+         return themeInfo != null && themeInfo.isEnabled();
+     }
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
