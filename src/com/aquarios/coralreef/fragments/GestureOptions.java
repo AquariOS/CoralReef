28b01bb592dcf6423e90832501b6c6bdd507d617
calebcabob <calphonic@gmail.com>
2019-02-14 23:46:13 -0500

    Revert "Move gestures to CoralReef [2/2]"
    This reverts commit 435bd8798d2fb012b651801d13000b0e2d4fca0a.

--- src/com/aquarios/coralreef/fragments/GestureOptions.java
+++ src/com/aquarios/coralreef/fragments/GestureOptions.java
@@ -19,77 +19,60 @@ package com.aquarios.coralreef.fragments;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.Settings;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.gestures.GestureSettings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class GestureOptions extends GestureSettings {
+import com.android.internal.logging.nano.MetricsProto;
 
-    private static final String TAG = "Gestures";
-    private static final String DOUBLE_TAP_STATUS_BAR_TO_SLEEP = "double_tap_sleep_gesture";
-    private static final String DOUBLE_TAP_LOCK_SCREEN_TO_SLEEP = "double_tap_sleep_anywhere";
+public class GestureOptions extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-    private ContentResolver mContentResolver;
-    private SwitchPreference mDoubleTapStatusBarToSleep;
-    private SwitchPreference mDoubleTapLockScreenToSleep;
+     SwitchPreference mDoubleTapToSleepEnabled;
+     SwitchPreference mDoubleTapToSleepAnywhere;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gesture_options);
+
+        mDoubleTapToSleepEnabled = (SwitchPreference) findPreference("double_tap_sleep_gesture");
+        mDoubleTapToSleepEnabled.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1);
+        mDoubleTapToSleepEnabled.setOnPreferenceChangeListener(this);
+
+        mDoubleTapToSleepAnywhere = (SwitchPreference) findPreference("double_tap_sleep_anywhere");
+        mDoubleTapToSleepAnywhere.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE, 0) == 1);
+        mDoubleTapToSleepAnywhere.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		       if (preference.equals(mDoubleTapToSleepEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, enabled ? 1 : 0);
+            return true;
+        }
+        if (preference.equals(mDoubleTapToSleepAnywhere)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE, enabled ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.AQUA;
-    }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        super.onCreatePreferences(savedInstanceState, rootKey);
-        mContentResolver = getActivity().getContentResolver();
-        mDoubleTapStatusBarToSleep = (SwitchPreference) findPreference(DOUBLE_TAP_STATUS_BAR_TO_SLEEP);
-        mDoubleTapLockScreenToSleep = (SwitchPreference) findPreference(DOUBLE_TAP_LOCK_SCREEN_TO_SLEEP);
-        updatePreferences();
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (mDoubleTapStatusBarToSleep == preference) {
-            updateDoubleTapStatusBarToSleep(true);
-            return true;
-        } else if (mDoubleTapLockScreenToSleep == preference) {
-            updateDoubleTapLockScreenToSleep(true);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    private void updateDoubleTapStatusBarToSleep(boolean clicked) {
-        if (clicked) {
-            Settings.System.putIntForUser(mContentResolver,
-                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
-                    (mDoubleTapStatusBarToSleep.isChecked() ? 1 : 0),
-                    UserHandle.USER_CURRENT);
-        }
-        boolean isDoubleTapStatusBarToSleep = (Settings.System.getIntForUser(
-                mContentResolver, Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
-                0, UserHandle.USER_CURRENT) == 1);
-        mDoubleTapStatusBarToSleep.setChecked(isDoubleTapStatusBarToSleep);
-    }
-
-    private void updateDoubleTapLockScreenToSleep(boolean clicked) {
-        if (clicked) {
-            Settings.System.putIntForUser(mContentResolver,
-                    Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE,
-                    (mDoubleTapLockScreenToSleep.isChecked() ? 1 : 0),
-                    UserHandle.USER_CURRENT);
-        }
-        boolean isDoubleTapLockScreenToSleep = (Settings.System.getIntForUser(
-                mContentResolver, Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE,
-                0, UserHandle.USER_CURRENT) == 1);
-        mDoubleTapLockScreenToSleep.setChecked(isDoubleTapLockScreenToSleep);
-    }
-
-    private void updatePreferences() {
-        updateDoubleTapStatusBarToSleep(false);
-        updateDoubleTapLockScreenToSleep(false);
+        return MetricsProto.MetricsEvent.AQUA;
     }
 }
