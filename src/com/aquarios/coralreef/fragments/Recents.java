bb725ba505112e0e7e8cafbfe4edab32b21d7c6a
calebcabob <calphonic@gmail.com>
2019-03-30 15:13:52 -0400

    Recents: Disable clear-all options when user is using pie style recents
    We can also do the same thing for pie recents, if we end up working something that isn't compatible with oreo recents
    
    Change-Id: I18e88b8ff62cd8ff8a45bb43a1a0de56fe4df601

--- src/com/aquarios/coralreef/fragments/Recents.java
+++ src/com/aquarios/coralreef/fragments/Recents.java
@@ -45,10 +45,12 @@ public class Recents extends SettingsPreferenceFragment implements
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String CATEGORY_OREO_STYLE_OPTIONS = "category_oreo_style_options";
 
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
     private ListPreference mRecentsComponentType;
+    private PreferenceCategory mOreoStyleOptions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,6 +74,10 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentsComponentType.setValue(String.valueOf(type));
         mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
         mRecentsComponentType.setOnPreferenceChangeListener(this);
+
+        // Hide clear-all options if set to Pie/horizontal style
+        mOreoStyleOptions = (PreferenceCategory) findPreference(CATEGORY_OREO_STYLE_OPTIONS);
+        updateOreoClearAll(type == 1);
     }
 
     @Override
@@ -79,9 +85,9 @@ public class Recents extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.AQUA;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private void updateOreoClearAll(boolean isOreo) {
+        mOreoStyleOptions.setEnabled(isOreo);
+        mOreoStyleOptions.setSelectable(isOreo);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -102,6 +108,7 @@ public class Recents extends SettingsPreferenceFragment implements
                Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
+            updateOreoClearAll(type == 1);
         return true;
         }
      return false;
