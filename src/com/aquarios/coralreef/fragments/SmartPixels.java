8700899535f07e594515d1cd35c98f279ecf0b37
Adin Kwok <adin.kwok@carbonrom.org>
2019-05-16 22:11:36 -0400

    Update Smart Pixels menu
    Menu updates preferences when changed externally via tile
    Now allows user to enable/disable auto-enable.
    
    *DU Edits*
    - Removed the check for the bool to enable smart pixel for the AOSP
    way which is using a preference controller (see Settings)
    
    - Added footer warning
    
    - Changed some strings up and added summary to burn-in preference
    
    Change-Id: Iaf78a60e5f083fde3c6fe562623169c01ba2ea22

--- src/com/aquarios/coralreef/fragments/SmartPixels.java
+++ src/com/aquarios/coralreef/fragments/SmartPixels.java
@@ -19,9 +19,11 @@ package com.aquarios.coralreef.fragments;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
-import android.os.PowerManager;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
@@ -30,21 +32,33 @@ import android.support.v7.preference.PreferenceManager;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import com.aquarios.support.preferences.SystemSettingSwitchPreference;
 
-public class SmartPixels extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
-    private static final String TAG = "SmartPixels";
+import java.util.ArrayList;
+import java.util.List;
 
+public class SmartPixels extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable {
+    private static final String TAG = "SmartPixels";
+    private static final String ENABLE = "smart_pixels_enable";
     private static final String ON_POWER_SAVE = "smart_pixels_on_power_save";
 
+    private Handler mHandler = new Handler();
+    private SmartPixelsObserver mSmartPixelsObserver;
+    private SystemSettingSwitchPreference mSmartPixelsEnable;
     private SystemSettingSwitchPreference mSmartPixelsOnPowerSave;
+
+    private boolean mIsSmartPixelsEnabled;
+    private boolean mIsSmartPixelsOnPowerSave;
 
     ContentResolver resolver;
 
@@ -54,21 +68,13 @@ public class SmartPixels extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.smart_pixels);
 
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_pixels_warning_text);
+
         resolver = getActivity().getContentResolver();
 
+        mSmartPixelsEnable = (SystemSettingSwitchPreference) findPreference(ENABLE);
         mSmartPixelsOnPowerSave = (SystemSettingSwitchPreference) findPreference(ON_POWER_SAVE);
-
-        updateDependency();
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-
-        boolean enableSmartPixels = getContext().getResources().
-                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
-        Preference SmartPixels = findPreference("smart_pixels");
-
-        if (!enableSmartPixels){
-            prefSet.removePreference(SmartPixels);
-        }
+        mSmartPixelsObserver = new SmartPixelsObserver(mHandler);
     }
 
     @Override
@@ -79,29 +85,82 @@ public class SmartPixels extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+        if (mSmartPixelsObserver != null) {
+            mSmartPixelsObserver.register();
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        if (mSmartPixelsObserver != null) {
+            mSmartPixelsObserver.unregister();
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
-        updateDependency();
         return true;
     }
 
-    private void updateDependency() {
-        boolean mUseOnPowerSave = (Settings.System.getIntForUser(
+    private void updatePreferences() {
+        mIsSmartPixelsEnabled = (Settings.System.getIntForUser(
+                resolver, Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT) == 1);
+        mIsSmartPixelsOnPowerSave = (Settings.System.getIntForUser(
                 resolver, Settings.System.SMART_PIXELS_ON_POWER_SAVE,
                 0, UserHandle.USER_CURRENT) == 1);
-        PowerManager pm = (PowerManager)getActivity().getSystemService(Context.POWER_SERVICE);
-        if (pm.isPowerSaveMode() && mUseOnPowerSave) {
-            mSmartPixelsOnPowerSave.setEnabled(false);
-        } else {
-            mSmartPixelsOnPowerSave.setEnabled(true);
+
+        mSmartPixelsEnable.setChecked(mIsSmartPixelsEnabled);
+        mSmartPixelsOnPowerSave.setChecked(mIsSmartPixelsOnPowerSave);
+    }
+
+    private class SmartPixelsObserver extends ContentObserver {
+        public SmartPixelsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void register() {
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SMART_PIXELS_ENABLE),
+                    false, this, UserHandle.USER_CURRENT);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SMART_PIXELS_ON_POWER_SAVE),
+                    false, this, UserHandle.USER_CURRENT);
+        }
+
+        public void unregister() {
+            resolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            updatePreferences();
         }
     }
 
+    /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                 @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    if (!(context.getResources().getBoolean(com.android.internal.R.bool.config_enableSmartPixels)))
+                        return new ArrayList<SearchIndexableResource>();
+
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.smart_pixels;
+                    result.add(sir);
+                    return result;
+                }
+                 @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
