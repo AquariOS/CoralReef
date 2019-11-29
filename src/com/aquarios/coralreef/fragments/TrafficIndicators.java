d680fbf79d9fdab2a69ca0d2adf056318e3f44fe
spezi77 <spezi7713@gmx.net>
2019-11-29 14:26:47 +0100

    NetworkTraffic positioning: Adapt to new settings flags.
    Partially based on:
    https://github.com/calebcabob/CoralReef/commit/1146bf83de9d72eb2d153bd87bbe0898225381a3
    
    spezi77 edits:
    -Clean up the code
    -Refactorized to work with our new commit: https://github.com/PixelDust-Project-X/android_frameworks_base/commit/c11cb77923fd09b866bb3d54a3a6a708034c09af
    
    Change-Id: I5fc55ed94a1c9fabacde19e128baaa172a4b4a4c
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/com/aquarios/coralreef/fragments/TrafficIndicators.java
+++ src/com/aquarios/coralreef/fragments/TrafficIndicators.java
@@ -60,9 +60,7 @@ public class TrafficIndicators extends SettingsPreferenceFragment implements
 
         mNetTrafficLocation = (ListPreference) findPreference("network_traffic_location");
         int location = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        mNetTrafficLocation.setValue(String.valueOf(location));
-        mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntry());
+                Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, 0, UserHandle.USER_CURRENT);
         mNetTrafficLocation.setOnPreferenceChangeListener(this);
 
         int value = Settings.System.getIntForUser(resolver,
@@ -72,7 +70,16 @@ public class TrafficIndicators extends SettingsPreferenceFragment implements
         mThreshold.setOnPreferenceChangeListener(this);
         mShowArrows = (SystemSettingSwitchPreference) findPreference("network_traffic_arrow");
 
-        updateTrafficLocation(location);
+        int netMonitorEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.NETWORK_TRAFFIC_STATE, 0, UserHandle.USER_CURRENT);
+        if (netMonitorEnabled == 1) {
+            mNetTrafficLocation.setValue(String.valueOf(location+1));
+            updateTrafficLocation(location+1);
+        } else {
+            mNetTrafficLocation.setValue("0");
+            updateTrafficLocation(0); 
+        }
+        mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntry());
     }
 
     @Override
@@ -89,10 +96,20 @@ public class TrafficIndicators extends SettingsPreferenceFragment implements
         if (preference == mNetTrafficLocation) {
             int location = Integer.valueOf((String) newValue);
             int index = mNetTrafficLocation.findIndexOfValue((String) newValue);
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_LOCATION, location, UserHandle.USER_CURRENT);
             mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntries()[index]);
-            updateTrafficLocation(location);
+            if (location > 0) {
+                // Convert the selected location mode from our list {0,1,2} and store it to "view location" setting: 0=sb; 1=expanded sb
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, location-1, UserHandle.USER_CURRENT);
+                // And also enable the net monitor
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT);
+                updateTrafficLocation(location+1);
+            } else { // Disable net monitor completely
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        Settings.System.NETWORK_TRAFFIC_STATE, 0, UserHandle.USER_CURRENT);
+                updateTrafficLocation(location);
+            }
             return true;
         } else if (preference == mThreshold) {
             int val = (Integer) newValue;
@@ -105,32 +122,17 @@ public class TrafficIndicators extends SettingsPreferenceFragment implements
     }
 
     public void updateTrafficLocation(int location) {
-        switch(location){ 
+        switch(location){
             case 0:
                 mThreshold.setEnabled(false);
                 mShowArrows.setEnabled(false);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_STATE, 0);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_EXPANDED_STATUS_BAR_STATE, 0);
                 break;
             case 1:
-                mThreshold.setEnabled(true);
-                mShowArrows.setEnabled(true);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_STATE, 1);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_EXPANDED_STATUS_BAR_STATE, 0);
-                break;
             case 2:
                 mThreshold.setEnabled(true);
                 mShowArrows.setEnabled(true);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_STATE, 0);
-                Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_EXPANDED_STATUS_BAR_STATE, 1);
                 break;
-            default: 
+            default:
                 break;
         }
     }
